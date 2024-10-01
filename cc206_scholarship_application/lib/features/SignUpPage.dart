26e1531ef95d830d16eea44f4ea2ec724707d89a
Augustine Seve F. Kang Bacale <augustineseve.kangbacale@wvsu.edu.ph>
2024-10-02 00:46:48 +0800

    New New Sign Up Page

--- cc206_scholarship_application/lib/features/SignUpPage.dart
+++ cc206_scholarship_application/lib/features/SignUpPage.dart
@@ -1,55 +1,170 @@
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+void main() {
+  runApp(const MyApp());
+}
 
-  @override 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sign Up Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SignUpPage(),
+    );
+  }
+}
+
+class SignUpPage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-           Icon(
-            Icons.person,
-            color: Colors.blue,
-            size: 200,
-          ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 60.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Sign up',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+             
+              Image.asset('assets/images/scholarship.png',
+                height: 300,),
+              SizedBox(height: 20),
 
-          Text(
-            'Sign Up',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-         
-          // FIrst Name TextFormField
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'First Name',
-            ),
-          ),
-         
-         // Last Name TextFormField
-         TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Last Name',
-            ),
-          ),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    prefixIcon: Icon(Icons.person),
+                    contentPadding: EdgeInsets.symmetric(vertical: 8.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
 
-          // Email TextFormField
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-            ),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.email),
+                    contentPadding: EdgeInsets.symmetric(vertical: 8.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Phone number',
+                    prefixIcon: Icon(Icons.phone),
+                    contentPadding: EdgeInsets.symmetric(vertical: 8.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                    contentPadding: EdgeInsets.symmetric(vertical: 8.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+             
+              SizedBox(height:5),
+             
+              SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () {
+                    },
+                    child: Text(
+                      "Log in",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-         
-          // Password TextFormField
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Password',
-            ),
-            obscureText: true,
-          ),
-        ],
+        ),
       ),
     );
-
   }
 }
